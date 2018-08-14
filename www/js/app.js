49566b565d73458b53eb73e05e1808bedfbc6bc9
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-08-13 22:28:32 -0300

    Lista dinâmica

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,163 @@
- $(document).ready(function(){
-    $('.modal').modal({
-    	outDuration: 100,
-    	inDuration: 100
+$(document).ready(function () {
+  $('.modal').modal({
+    outDuration: 100,
+    inDuration: 100
+  });
+});
+
+
+$(document).ready(function () {
+  $('input#input_text, textarea#textarea2').characterCounter();
+});
+
+$('.dropdown-trigger').dropdown();
+
+$(document).ready(function () {
+  $('.fixed-action-btn').floatingActionButton({
+    direction: 'left',
+    hoverEnabled: false
+  });
+});
+
+// lista de tarefas
+
+$(function () {
+  var meuLogin = "teste@email.com";
+  var server = "http://livro-capitulo07.herokuapp.com";
+
+  var $lastClicked;
+
+  function onTarefaDeleteClick() {
+    $(this).parent(".tarefa-item").off("click").hide("slow", function () {
+      $.post(server + "/tarefa",
+        {
+          usuario: meuLogin,
+          _method: "DELETE",
+          tarefa_id: $(this).children(".tarefa-id").text()
+        });
+
+      $(this).remove();
     });
-  });
+  }
 
+  function onTarefaItemClick() {
+    if (!$(this).is($lastClicked)) {
+      if ($lastClicked !== undefined) {
+        savePendingEdition($lastClicked);
+      }
 
-  $(document).ready(function() {
-    $('input#input_text, textarea#textarea2').characterCounter();
-  });
-  
-  $('.dropdown-trigger').dropdown();
+      $lastClicked = $(this);
 
-  $(document).ready(function(){
-    $('.fixed-action-btn').floatingActionButton({
-    	direction: 'left',
-    	hoverEnabled: false
-    });
-  });
+      var text = $lastClicked.children(".tarefa-texto").text();
+      // guardamos o id da tarefa na edi��o
+      var id = $lastClicked.children(".tarefa-id").text();
+
+      var html = "<div class='tarefa-id'>" + id + "</div>" +
+        "<input type='text' " + "class='tarefa-edit' value='" + text + "'>";
+
+      $(this).html(html);
+
+      $(".tarefa-edit").keydown(onTarefaEditKeydown);
+    }
+  }
+
+  function onTarefaKeyDown() {
+    if (event.which === 13) {
+      addTarefa($("#tarefa").val());
+      $("#tarefa").val("");
+    }
+  }
+
+  function onTarefaEditKeydown(event) {
+    if (event.which === 13) {
+      savePendingEdition($lastClicked);
+      $lastClicked = undefined;
+    }
+  }
+
+  function addTarefa(text, id) {
+    id = id || 0;
+
+    var $tarefa = $("<div />")
+      .addClass("tarefa-item")
+      .append($("<div />")
+        .addClass("tarefa-id")
+        .text(id))
+      .append($("<div />")
+        .addClass("tarefa-texto")
+        .text(text))
+      .append($("<div />")
+        .addClass("tarefa-delete"))
+      .append($("<div />")
+        .addClass("clear"));
+
+    $("#tarefa-list").append($tarefa);
+
+    $(".tarefa-delete").click(onTarefaDeleteClick);
+
+    $(".tarefa-item").click(onTarefaItemClick);
+
+    if (id === 0) {
+      var div = $tarefa.children(".tarefa-id");
+      console.log("id", div);
+      newTarefa(text, $(div));
+    }
+  }
+
+  function savePendingEdition($tarefa) {
+    var text = $tarefa.children(".tarefa-edit").val();
+    var id = $tarefa.children(".tarefa-id").text();
+
+    $tarefa.empty();
+
+    $tarefa.append("<div class='tarefa-id'>" + id + "</div>")
+      .append("<div class='tarefa-texto'>" + text + "</div>")
+      .append("<div class='tarefa-delete'></div>")
+      .append("<div class='clear'></div>");
+
+    updateTarefa(text, id);
+
+    $(".tarefa-delete").click(onTarefaDeleteClick);
+
+    $tarefa.click(onTarefaItemClick);
+  }
+
+  function loadTarefas() {
+    $("#tarefa").empty();
+
+    $.getJSON(server + "/tarefas", { usuario: meuLogin })
+      .done(function (data) {
+        console.log("data: ", data);
+
+        for (var tarefa = 0; tarefa < data.length; tarefa++) {
+          addTarefa(data[tarefa].texto, data[tarefa].id);
+        }
+      });
+  }
+
+  function updateTarefa(texto, id) {
+    $.post(server + "/tarefa", { tarefa_id: id, texto: texto })
+  }
+
+  function newTarefa(text, $div) {
+    $.post(server + "/tarefa",
+      {
+        usuario: meuLogin,
+        texto: text,
+        _method: "PUT"
+      })
+      .done(function (data) {
+        console.log($div);
+        $div.text(data.id);
+      });
+  }
+
+  $("#tarefa").keydown(onTarefaKeyDown);
+
+  $(".tarefa-delete").click(onTarefaDeleteClick);
+
+  $(".tarefa-item").click(onTarefaItemClick);
+
+  loadTarefas();
+}
+);
